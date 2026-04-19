c486b44ac625dd9f511cc594afae377e74fb052a
Shriram <jadhawarshriramking@gmail.com>
2026-04-19 23:18:05 +0530

    practice

--- Assignment_4/CountRows.py
+++ Assignment_4/CountRows.py
@@ -1,10 +1,11 @@
+import json
 import csv
 
 def count_rows_in_csv(file_name):
     try:
         with open(file_name, mode='r', newline='') as file:
             reader = csv.reader(file)
-            row_count = sum(1 for row in reader)  # Counts each row in the file
+            row_count = sum(1 for row in reader)  
         return row_count
     except FileNotFoundError:
         print(f"The file '{file_name}' was not found.")
