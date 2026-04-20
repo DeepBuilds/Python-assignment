dc735d10b697dbfeee72e734902ddfcb1e2d8ce4
Shriram <jadhawarshriramking@gmail.com>
2026-04-20 23:40:43 +0530

    new json to cvs

--- Practice/json_cvs.py
+++ Practice/json_cvs.py
@@ -1,15 +1,16 @@
-import json 
+import json
 import csv
-def json_to_csv(json_data,csv_file_name):
-    with open(csv_file_name, mode='w',newline='') as csv_file:
-        writer= csv.DictWriter(csv_file,fieldnames=json_data[0].keys())
+def json_to_csv(json_data,csv_name):
+    with open(csv_name,mode='w',newline='') as csv_file:
+        writer=csv.DictWriter(csv_file,fieldnames=json_data[0].keys())
         writer.writeheader()
         writer.writerows(json_data)
-        print("File created",csv_file_name)
+        print(f"CSV file created as {csv_name}")
 if __name__=="__main__":
     json_data=[
-        {"name": "John", "age": 30, "city": "New York"},
-        {"name": "Anna", "age": 22, "city": "London"},
-        {"name": "Mike", "age": 32, "city": "San Francisco"}
-    ]
-    json_to_csv(json_data,"practigce.csv")
+        {'name':"shriram","age":18,"gender":'M'},
+        {'name':"karan","age":15,"gender":'M'},
+        {'name':"marnan","age":19,"gender":'M'}
+        ]
+    csv_file=input("Enter CSV file:- ")+".csv"
+    json_to_csv(json_data,csv_file)
